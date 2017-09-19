13249135bdfa64d9792491728cba1b3e41f5fa70
Philippe Rivière <fil@rezo.net>
2017-09-18 17:16:26 -0700

    test

--- test/clip/polygon-test.js
+++ test/clip/polygon-test.js
@@ -1,7 +1,13 @@
 var tape = require("tape"),
-    d3 = require("../../");
+    d3 = require("../../"),
+    d3_geo = require("d3-geo");
 
-tape("d3.geoClipPolygon is not yet implemented", test => {
-  test.throws(d3.geoClipPolygon, /not yet implemented/);
+tape("clipPolygon clips line", function(test) {
+  var clipPolygon = d3.geoClipPolygon({ type: "Polygon", coordinates: [[[-10, -10], [-10, 10], [10, 10], [10, -10], [-10, -10]]] });
+  var projection = d3_geo.geoEquirectangular().clipAngle(10).preclip(clipPolygon);
+  var path = d3_geo.geoPath()
+    .projection(projection);
+  test.equal(path({type:"LineString", coordinates:[[-20,0], [20,0]]}), path({type:"LineString", coordinates:[[-10.5,0], [10.5,0]]}));
   test.end();
 });
+
