2649e4da196518e9cfab2c896b300771f92d8311
Philippe Rivière <fil@rezo.net>
2018-03-04 14:30:47 +0100

    d3-geo-polygon exports its own versions of geoPolyhedral, geoPolyhedralButterfly, geoPolyhedralCollignon and geoPolyhedralWaterman, with minimal code duplication.
    With this version, https://github.com/d3/d3-geo-projection/pull/132 is not needed anymore.
    
    (If you need to use both d3-geo-projection and d3-geo-polygon, they should be included in that order.)

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 export {default as geoClipPolygon} from "./src/clip/polygon";
+export {default as geoPolyhedral} from "./src/polyhedral/index";
+export {default as geoPolyhedralButterfly} from "./src/polyhedral/butterfly.js";
+export {default as geoPolyhedralCollignon} from "./src/polyhedral/collignon.js";
+export {default as geoPolyhedralWaterman} from "./src/polyhedral/waterman.js";
+
+// if necessary, the following line could export a copy of the d3-geo-projection versions under the names xxxxUnclipped
+// export {geoPolyhedral as geoPolyhedralUnclipped, geoPolyhedralButterfly as geoPolyhedralButterflyUnclipped, geoPolyhedralCollignon as geoPolyhedralCollignonUnclipped, geoPolyhedralWaterman as geoPolyhedralWatermanUnclipped} from "./node_modules/d3-geo-projection/index";
+
+// if necessary, the following line could export a copy of these versions under the names xxxxClipped
+// export {default as geoPolyhedral, default as geoPolyhedralClipped} from "./src/polyhedral/index";
+// export {default as geoPolyhedralButterfly, default as geoPolyhedralButterflyClipped} from "./src/polyhedral/butterfly.js";
+// export {default as geoPolyhedralCollignon, default as geoPolyhedralCollignonClipped} from "./src/polyhedral/collignon.js";
+// export {default as geoPolyhedralWaterman, default as geoPolyhedralWatermanClipped} from "./src/polyhedral/waterman.js";
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,18 @@
 export default {
   input: "index",
   external: [
-    "d3-array"
+    "d3-array",
+    "d3-geo",
+    "d3-geo-projection"
   ],
   output: {
     extend: true,
     file: "build/d3-geo-polygon.js",
     format: "umd",
     globals: {
-      "d3-array": "d3"
+      "d3-array": "d3",
+      "d3-geo": "d3",
+      "d3-geo-projection": "d3"
     },
     name: "d3"
   }
