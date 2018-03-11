31c70e61b6ded9c8e16781a3f25b88446dd694f1
Philippe Rivière <fil@rezo.net>
2018-03-11 10:41:49 +0100

    test inverse polyhedrals

---
+++ test/invert-test.js
@@ -0,0 +1,19 @@
+var tape = require("tape"),
+    d3 = require("../"),
+    d3_geo = require("d3-geo");
+require("./projectionEqual");
+
+
+tape("inverse polyhedrals", function(test) {
+
+  [ d3.geoAirocean(), d3.geoCubic(), d3.geoIcosahedral(), d3.geoDodecahedral() ]
+  .forEach(function(projection) {
+    [ [-23, 12], [10,10], [100,-45] ]
+    .forEach(function(location) {
+      projection.angle(Math.random()*360);
+      test.projectionEqual(projection, location, projection(location), 1e-5);
+    });
+  });
+  test.end();
+});
+
