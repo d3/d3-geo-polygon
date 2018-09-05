6de3b64a21899ca09641df3dd84ee528ba7ee178
Philippe Rivière <fil@rezo.net>
2018-09-06 01:12:58 +0200

    update the build toolchain

---
+++ src/index.js
@@ -0,0 +1,14 @@
+export {default as geoClipPolygon} from "./clip/polygon";
+export {default as geoPolyhedral} from "./polyhedral/index";
+export {default as geoPolyhedralButterfly} from "./polyhedral/butterfly";
+export {default as geoPolyhedralCollignon} from "./polyhedral/collignon";
+export {default as geoPolyhedralWaterman} from "./polyhedral/waterman";
+export {default as geoPolyhedralVoronoi} from "./polyhedral/voronoi";
+export {default as geoDodecahedral} from "./polyhedral/dodecahedral";
+export {default as geoCox, coxRaw as geoCoxRaw} from "./cox.js";
+export {default as geoTetrahedralLee, leeRaw as geoLeeRaw} from "./tetrahedralLee.js";
+export {default as geoGrayFullerRaw} from "./grayfuller";
+export {default as geoAirocean} from "./airocean";
+export {default as geoIcosahedral} from "./icosahedral";
+export {default as geoCubic} from "./cubic";
+export {default as geoCahillKeyes, cahillKeyesRaw as geoCahillKeyesRaw} from "./cahillKeyes";
