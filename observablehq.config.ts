94ec69c64e0798453f3f25d3da948653dac687be
Philippe Rivière <fil@rezo.net>
2024-10-07 21:17:54 +0200

    dynamicPaths snapshots

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -1,5 +1,13 @@
+import * as snapshots from "./test/snapshots.js";
+
 export default {
   title: "d3-geo-polygon",
-  src: "root",
-  output: "dist/docs"
+  src: "docs",
+  output: "dist/docs",
+  async *dynamicPaths() {
+    for (const snapshot of Object.keys(snapshots)) {
+      yield `/snapshots/${snapshot}.png`;
+      yield `/snapshots/${snapshot}-dark.png`;
+    }
+  },
 };
